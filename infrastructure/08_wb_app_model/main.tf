c71256d1986f53cd4653bb196af772f5554d0ae3
christine fang <christinefang@christinedeMacBook-Pro.local>
2024-08-16 00:50:27 +0200

    feat: create model web app and active continuous deployment

--- infrastructure/08_wb_app_model/main.tf
+++ infrastructure/08_wb_app_model/main.tf
@@ -49,7 +49,7 @@ resource "azurerm_role_assignment" "acr_pull" {
   depends_on = [
     azurerm_linux_web_app.model
   ]  
-  principal_id   = azurerm_linux_web_app.model.identity.principal_id
+  principal_id   = azurerm_linux_web_app.model.identity[0].principal_id
   role_definition_name = "AcrPull"
   scope          = data.azurerm_container_registry.acr.id
 }
